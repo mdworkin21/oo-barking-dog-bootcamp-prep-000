32e1efaf2c6867c9e3f61220fbd830f12795b9fc
Matthew Dworkin <mdworkin21@gmail.com>
2018-01-15 20:16:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,17 @@
 # Your code goes here!
 class Dog
+  
   def name=(name)
     @name = name
   end
 
-  def name (name)
+  def name 
     @name
   end
 
-  def bark
+  
+
+  def bark 
     puts "Woof!"
   end
 
